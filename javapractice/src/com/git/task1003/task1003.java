4797d5a767dbdfad12689d9830f0bfd9f1cd0614
Ruhul Abedin <ruhul.abedin@gmail.com>
2015-07-03 03:47:28 -0700

    git commit for task#1003

---
+++ javapractice/src/com/git/task1003/task1003.java
@@ -0,0 +1,10 @@
+package com.git.task1003;
+
+public class task1003 {
+	public static void main(String[] args) {
+		System.out.println("for loop");
+		for (int i = 0; i < 10; i++) {
+			System.out.println(++i);
+		}
+	}
+}
