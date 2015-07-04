72a72daa03d13a26ac66a9fb57d7112d0cac580a
Ruhul Abedin <ruhul.abedin@gmail.com>
2015-07-04 03:43:11 -0700

    add commit

--- javapractice/src/com/java/equalsandhashcode/EmployeeTest.java
+++ javapractice/src/com/java/equalsandhashcode/EmployeeTest.java
@@ -1,21 +1,28 @@
 package com.java.equalsandhashcode;
 
+import java.util.HashMap;
+
 public class EmployeeTest {
 	public static void main(String[] args) {
-      Employee emp = new Employee();
-      emp.setEmpid(10);
-      emp.setFirstname("ruhul");
-      emp.setLastname("abedin");
-      emp.setEmail("ruhul.abedin@gmail.com");
-      
-      Employee emp1 = new Employee();
-      emp1.setEmpid(10);
-      emp1.setFirstname("ruhul");
-      emp1.setLastname("abedin");
-      emp1.setEmail("ruhul.abedin@gmail.com");
-      
-      System.out.println(emp.equals(emp1));
-      System.out.println(emp.hashCode());
-      System.out.println(emp1.hashCode());
+		Employee emp = new Employee();
+		emp.setEmpid(10);
+		emp.setFirstname("ruhul");
+		emp.setLastname("abedin");
+		emp.setEmail("ruhul.abedin@gmail.com");
+
+		Employee emp1 = new Employee();
+		emp1.setEmpid(10);
+		emp1.setFirstname("ruhul");
+		emp1.setLastname("abedin");
+		emp1.setEmail("ruhul.abedin@gmail.com");
+
+		System.out.println(emp.equals(emp1));
+		System.out.println(emp.hashCode());
+		System.out.println(emp1.hashCode());
+
+		HashMap<Employee, Employee> empMap = new HashMap<Employee, Employee>();
+		empMap.put(emp, emp);
+		empMap.put(emp1, emp1);
+		System.out.println(empMap.size());
 	}
 }
